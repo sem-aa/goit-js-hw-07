cd1227fb2f448cfa12cb412d11e7bb32732ea74c
sem-aa <66129330+sem-aa@users.noreply.github.com>
2020-11-09 23:17:52 +0200

    Выполняет задание 2

---
+++ js/task-2.js
@@ -0,0 +1,22 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+  const list = document.querySelector('#ingredients')
+  
+
+  const createElement = ingredients.map(ingredient => {
+   const listEl =  document.createElement('li');
+   listEl.textContent = ingredient;
+return listEl
+
+});
+
+
+list.append(...createElement);
+console.log(createElement)
