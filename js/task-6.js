219d5d109bf95080fa97ba51e4cee9ae8cc34146
sem-aa <66129330+sem-aa@users.noreply.github.com>
2020-11-11 21:19:23 +0200

    выполняет задание 7

--- js/task-6.js
+++ js/task-6.js
@@ -9,9 +9,6 @@ validInput.addEventListener('blur', offFocusInput)
 function offFocusInput(event){
     const valueInput = event.currentTarget.value
     validInput.classList.add('invalid')
-    console.log('Теряет фокус')
-    console.log(valueInput)
-    console.log(valueInput.length)
     valueInput.length === dataLength ?
     validInput.classList.replace('invalid', 'valid')  : 
     validInput.classList.replace('valid', 'invalid')
---
+++ js/task-7.js
@@ -0,0 +1,7 @@
+const fontSizeEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text')
+fontSizeEl.addEventListener('input', changeSizeFont)
+function changeSizeFont(event){
+   
+    textEl.style.fontSize = event.currentTarget.value + 'px'
+}
